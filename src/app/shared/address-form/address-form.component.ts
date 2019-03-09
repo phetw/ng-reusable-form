34c9d3a6ee210e1014d6eaa15a21345a2d6860f6
rappad <wasuwat.se@gmail.com>
2019-03-09 19:12:28 +0700

    add country dropdown implementation

--- src/app/shared/address-form/address-form.component.ts
+++ src/app/shared/address-form/address-form.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from "@angular/core"
 import { FormGroup, FormBuilder, Validators } from "@angular/forms"
 
 import MockCompanyList from "../../core/mock/company-list.mock"
+import { CountryService } from "src/app/core/service/country/country.service"
+import { Observable } from "rxjs"
+import { ICountry } from "src/app/core/model/country.model"
 
 @Component({
   selector: "app-address-form",
@@ -11,27 +14,53 @@ import MockCompanyList from "../../core/mock/company-list.mock"
 export class AddressFormComponent implements OnInit {
   @Input() parentForm: FormGroup
 
-  addressForm: FormGroup
+  address: FormGroup
+  country$: Observable<ICountry[]>
   company: Array<string> = MockCompanyList
 
-  constructor(private _fb: FormBuilder) {}
+  constructor(
+    private _fb: FormBuilder,
+    private countryService: CountryService
+  ) {}
 
   ngOnInit() {
-    this.addressForm = this.createAddressFormInstance()
-    this.addAddressFormToParentForm(this.addressForm)
+    this.getCountry()
+    this.address = this.createAddressFormInstance()
+    this.addAddressFormToParentForm(this.address)
   }
 
   createAddressFormInstance(): FormGroup {
     return this._fb.group({
-      company: [null, Validators.required],
-      street: ["", Validators.required],
-      city: ["", Validators.required],
-      state: ["", Validators.required],
-      zip: ["", Validators.required],
+      company: [null, [Validators.required]],
+      street: ["", [Validators.required]],
+      city: ["", [Validators.required]],
+      state: ["", [Validators.required]],
+      country: [null, [Validators.required]],
+      zip: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(5),
+          Validators.pattern("^[0-9]*$"),
+        ],
+      ],
     })
   }
 
   addAddressFormToParentForm(addressForm: FormGroup): void {
-    this.parentForm.addControl("addressForm", addressForm)
+    this.parentForm.addControl("address", addressForm)
+  }
+
+  get zip() {
+    return this.address.get("zip")
+  }
+
+  getCountry() {
+    this.country$ = this.countryService.getCountryList()
+  }
+
+  onCountryChange($event) {
+    console.log($event.name)
   }
 }
