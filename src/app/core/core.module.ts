14f714a2177f48bdfeb47164ec667e553ba40403
rappad <wasuwat.se@gmail.com>
2019-03-09 19:13:02 +0700

    add httpclient to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from "@angular/core"
 import { CommonModule } from "@angular/common"
+import { HttpClientModule } from "@angular/common/http"
 import { NgSelectModule } from "@ng-select/ng-select"
 import { ReactiveFormsModule } from "@angular/forms"
 
-const exportedCoreModules = [ReactiveFormsModule, NgSelectModule]
+const exportedCoreModules = [
+  ReactiveFormsModule,
+  NgSelectModule,
+  HttpClientModule,
+]
 
 @NgModule({
-  declarations: [],
   imports: [CommonModule, ...exportedCoreModules],
-  exports: exportedCoreModules,
+  exports: [...exportedCoreModules],
 })
 export class CoreModule {}
